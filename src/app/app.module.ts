6ca68a3bd3d542c189ec75489f03b2e69a18830c
Fene-87 <fenekayas87@gmail.com>
2023-06-23 00:48:33 +0300

    Make rerquired installations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateRegistrationComponent } from './create-registration/create-registration.component';
+import { RegistrationListComponent } from './registration-list/registration-list.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { NgToastModule } from 'ng-angular-popup';
+import { NgConfirmModule } from 'ng-confirm-box';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRegistrationComponent,
+    RegistrationListComponent,
+    UserDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    NgToastModule,
+    NgConfirmModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
