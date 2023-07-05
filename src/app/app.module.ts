b6f9b6e0a41b8864663210d8d276e9652c0e9b3b
Fene-87 <fenekayas87@gmail.com>
2023-07-05 13:53:15 +0300

    Create User model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
 
     NgToastModule,
     NgConfirmModule,
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../model/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private baseUrl: string = 'http://localhost:3000/enquiries'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  postRegistration(registerObj: User) {
+    return this.http.post<User>(`${this.baseUrl}`, registerObj)
+  }
+
+  getRegisteredUser() {
+    return this.http.get<User[]>(`${this.baseUrl}`)
+  }
+
+  updateRegiteredUser(registerObj: User, id: number) {
+    return this.http.put<User>(`${this.baseUrl}/${id}`, registerObj)
+  }
+
+  deleteRegisteredUser(id: number) {
+    return this.http.delete<User>(`${this.baseUrl}/${id}`)
+  }
+
+  getRegisteredUserId(id: number) {
+    return this.http.get<User>(`${this.baseUrl}/${id}`)
+  }
 }
