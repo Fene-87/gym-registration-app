1e73a52993731db8738e528669a3482eb1428cee
Fene-87 <fenekayas87@gmail.com>
2023-07-06 17:21:08 +0300

    Create enquiries table to display a list of all enquiries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,9 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -32,7 +35,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    
+
     NgToastModule,
     NgConfirmModule,
     MatToolbarModule,
@@ -44,6 +47,9 @@ import { HttpClientModule } from '@angular/common/http';
     MatSelectModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
     AppRoutingModule,
     ReactiveFormsModule,
   ],
