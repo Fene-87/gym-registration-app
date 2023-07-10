ecf52e0241d9085359ad0e035d9b11ec8075d8e7
Fene-87 <fenekayas87@gmail.com>
2023-07-11 00:06:07 +0300

    Create details page to show specific user's details

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user.model';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.scss']
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnInit {
+  public userId!: number;
+  userDetail!: User;
+
+  constructor(private activatedRoute: ActivatedRoute, private api: ApiService) {
+
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(val => {
+      this.userId = val['id'];
+      this.fetchUserDetails(this.userId)
+    })
+  }
+
+  fetchUserDetails(userId: number) {
+    this.api.getRegisteredUserId(this.userId)
+    .subscribe(res => {
+      this.userDetail = res;
+    })
+  }
 
 }
